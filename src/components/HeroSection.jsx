5b2bc32fcdf8816096542fce9abfe86ba614f3a8
NickNWJ <uarehup@gmail.com>
2025-10-27 05:05:41 +0800

    added react vite project folder (incomplete)

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,40 @@
+import { ArrowDown } from "lucide-react"
+
+export const HeroSection = () => {
+    return (
+        <section 
+            id="hero" 
+            className="relative min-h-screen flex flex-col items-center justify-center px-4"
+        >
+            <div className="space-y-6">
+                <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+                    <span className="opacity-0 animate-fade-in"> Hi, I'm</span>
+                    <span className="text-primary opacity-0 animate-fade-in-delay-1">
+                    {" "}
+                    Nick
+                    </span>
+                    <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
+                    {""}
+                    Ng
+                    </span>
+                </h1>
+
+                <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+                    I create website with modern technologies.
+                    Specializing in web development.
+                </p>
+
+                <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+                    <a href="#projects" className="cosmic-button">
+                    View My Work
+                    </a>
+                </div>
+
+                <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+                    <span> Scroll </span>
+                    <ArrowDown className="h5 w-5 text-primary" />
+                </div>
+            </div>
+        </section>
+    )
+}
